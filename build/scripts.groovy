c3cc9c11ffb3498477198af269e96937fb133518
Dmitriy Krasnikov <dmikov@gmail.com>
2020-05-15 15:49:49 -0400

    Cleaning up

---
+++ build/scripts.groovy
@@ -0,0 +1,26 @@
+def buildThis(projectFolder) {
+    if (!currentBuild.changeSets) {
+        return true
+    }
+    for (changeLogSet in currentBuild.changeSets) {
+        for (entry in changeLogSet.getItems()) {
+            for (file in entry.getAffectedFiles()) {
+                println(file.getPath())
+                if (file.getPath().contains(projectFolder)) {
+                    return true
+                }
+            }
+        }
+    }
+    return false
+}
+
+def printEnv() {
+    println(env.IMAGE_GROOVY_LOAD)
+}
+
+def printThis(value) {
+    println(value)
+}
+
+return this
